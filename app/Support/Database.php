1d3f1c51bebfdbee22f7eb7e0e9432cec713b3df
muhsina.akter <muhsina.akter2@gmail.com>
2020-08-03 00:00:18 +0600

    Database class design

--- app/Support/Database.php
+++ app/Support/Database.php
@@ -1,11 +1,36 @@
 <?php
 namespace Edu\Board\Support;
+require_once "../../config.php";
+use PDO;
 
 /**
  * Database Managements
  */
 
-class Database{
+abstract class Database{
+
+    /**
+     * Server Information
+     */
+
+    private  $host = HOST;
+    private  $user = USER;
+    private  $pass = PASS;
+    private  $db = DB;
+    private  $connection;
+
+    /**
+     * Database Connection
+     */
+
+    private function connection(){
+       //PDO Connection
+
+        $connection = new PDO("mysql:host =". $this -> host."; db_name=" . $this -> db, $this -> user,  $this -> pass);
+    }
+
 
 }
+
+
 ?>
